e07721afe585c6d0f87c541d901be218a2c3f6a8
Hiklajo <hielkevriesema@gmail.com>
2026-04-01 21:26:23 +0200

    Add files via upload

---
+++ scripts/princess.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+
+
+
+
+@onready var press_to_kiss: Sprite2D = $"../PressToKiss"
+@onready var particles: AnimatedSprite2D = $AnimatedSprite2D/particles
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var fade: CanvasLayer = $"../fade"
+
+var inside = false
+
+func _input(_event):
+	if Input.is_action_just_pressed("interact"):
+		if inside:
+			print("interactions")
+			inside = false
+			particles.play("particles")
+			
+			animated_sprite_2d.play("frog")
+			await get_tree().create_timer(4).timeout
+			
+			animated_sprite_2d.play("frog_idle")
+			await get_tree().create_timer(1.5).timeout
+
+			await fade.fade(1.0, 3).finished
+			get_tree().change_scene_to_file.call_deferred("res://scenes/logo.tscn")
+			
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	print("hit")
+	inside = true
+	$"../PressToKiss".visible = true
+ 	
+
+		
+func _on_area_2d_body_exited(_body: Node2D) -> void:
+	$"../PressToKiss".visible = false
+	inside = false
